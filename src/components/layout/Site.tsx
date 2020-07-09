0d6da20e64bdfac0454a5c68eb31cc89db8f3887
Karthikeyan <>
2020-07-09 19:47:39 +0530

    ui fixes

--- src/components/layout/Site.tsx
+++ src/components/layout/Site.tsx
@@ -40,41 +40,43 @@ const Site = (props) => {
           theme='dark'
         >
           <div className='brand'>FR<span>8</span></div>
-          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-            <Menu.Item key='1' icon={<DashboardOutlined />}>
-              <Link href='/'><a>Dashboard</a></Link>
-            </Menu.Item>
-            <Menu.Item key='2' icon={<SwapOutlined />}>
-              <Link href='/trips'><a>Trips</a></Link>
-            </Menu.Item>
-            <Menu.Item key='3' icon={<CarOutlined />}>
-              <Link href='/trucks'><a>Trucks</a></Link>
-            </Menu.Item>
-            <Menu.Item key='4' icon={<SmileOutlined />}>
-              <Link href='/partners'><a>Partners</a></Link>
-            </Menu.Item>
-            <Menu.Item key='5' icon={<TeamOutlined />}>
-              <Link href='/customers'><a>Customers</a></Link>
-            </Menu.Item>
-            <Menu.Item key='6' icon={<CreditCardOutlined />}>
-              <Link href='/cards'><a>Cads</a></Link>
-            </Menu.Item>
-            <Menu.Item key='7' icon={<NodeIndexOutlined />}>
-              <Link href='/branches'><a>Branches</a></Link>
-            </Menu.Item>
-            <Menu.Item key='8' icon={<CopyOutlined />}>
-              <Link href='/approvals'><a>Approvals</a></Link>
-            </Menu.Item>
-            <Menu.Item key='9' icon={<LineChartOutlined />}>
-              <Link href='/reports'><a>Reports</a></Link>
-            </Menu.Item>
-            <Menu.Item key='10' icon={<TransactionOutlined />}>
-              <Link href='/payables'><a>Payables</a></Link>
-            </Menu.Item>
-            <Menu.Item key='11' icon={<FullscreenOutlined />}>
-              <Link href='/sourcing'><a>Sourcing</a></Link>
-            </Menu.Item>
-          </Menu>
+          <div className='scrollFix'>
+            <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+              <Menu.Item key='1' icon={<DashboardOutlined />}>
+                <Link href='/'><a>Dashboard</a></Link>
+              </Menu.Item>
+              <Menu.Item key='2' icon={<SwapOutlined />}>
+                <Link href='/trips'><a>Trips</a></Link>
+              </Menu.Item>
+              <Menu.Item key='3' icon={<CarOutlined />}>
+                <Link href='/trucks'><a>Trucks</a></Link>
+              </Menu.Item>
+              <Menu.Item key='4' icon={<SmileOutlined />}>
+                <Link href='/partners'><a>Partners</a></Link>
+              </Menu.Item>
+              <Menu.Item key='5' icon={<TeamOutlined />}>
+                <Link href='/customers'><a>Customers</a></Link>
+              </Menu.Item>
+              <Menu.Item key='6' icon={<CreditCardOutlined />}>
+                <Link href='/cards'><a>Cads</a></Link>
+              </Menu.Item>
+              <Menu.Item key='7' icon={<NodeIndexOutlined />}>
+                <Link href='/branches'><a>Branches</a></Link>
+              </Menu.Item>
+              <Menu.Item key='8' icon={<CopyOutlined />}>
+                <Link href='/approvals'><a>Approvals</a></Link>
+              </Menu.Item>
+              <Menu.Item key='9' icon={<LineChartOutlined />}>
+                <Link href='/reports'><a>Reports</a></Link>
+              </Menu.Item>
+              <Menu.Item key='10' icon={<TransactionOutlined />}>
+                <Link href='/payables'><a>Payables</a></Link>
+              </Menu.Item>
+              <Menu.Item key='11' icon={<FullscreenOutlined />}>
+                <Link href='/sourcing'><a>Sourcing</a></Link>
+              </Menu.Item>
+            </Menu>
+          </div>
         </Sider>
         <Layout className={`${menuCollapse ? 'closeMenu' : 'openMenu'} ${fixed ? 'clearTop' : ''}`}>
           <Header className='header'>
@@ -90,7 +92,6 @@ const Site = (props) => {
               {props.children}
             </Card>
           </Content>
-          <Footer>Footer</Footer>
         </Layout>
       </Layout>
     </>
