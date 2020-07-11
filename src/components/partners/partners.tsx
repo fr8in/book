c745c157941ceda1fe4b735488ad9686a322c9da
Shilpaveronica <Shilpaveronica@bitbucket.org>
2020-07-11 10:47:50 +0530

    partner Table

--- src/components/partners/partners.tsx
+++ src/components/partners/partners.tsx
@@ -1,10 +1,63 @@
-
+import React from 'react'
+import {Table} from "antd";
+import mock from '../../../mock/partner/partnerData';
 import PageLayout from '../layout/PageLayout'
 
+
 const Partners = () => {
+  const columnsCurrent = [
+    {
+        title: "Partner Code",
+        dataIndex: "partnerCode",               
+    },
+    {
+        title: "Partner",
+        dataIndex: "name",
+    },
+    {
+        title: "Region",
+        dataIndex: "regionName",
+    },
+    {
+        title: "Contact No",
+        dataIndex: "mobileNo",
+    },
+    {
+        title: "Email",
+        dataIndex: "email",
+    },
+    {
+        title: "Avg Km/day",
+        dataIndex: "averageKm",
+        sorter: true
+    },
+    {
+        title: "Trucks",
+        dataIndex: "truckCount",
+    },
+    {
+        title: "Invoiced",
+        dataIndex: "invoiceAmount",  
+    },
+    {
+        title: "Invoice Pending",
+        dataIndex: "invoicePendingAmount",              
+    },
+    {
+        title: "Status",
+        dataIndex: "active",
+    },
+];
   return (
     <PageLayout title='Partners'>
-              Partners
+             <Table
+                columns={columnsCurrent}
+                dataSource={mock}
+                rowKey={record => record.id}
+                size='middle'
+                scroll={{ x: 800, y: 400 }}
+                pagination={false}
+            />
     </PageLayout>
   )
 }
