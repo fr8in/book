3790b7ec17318ef23353c9763f9788b2ca0119a6
Karthikeyan <>
2020-07-14 12:04:47 +0530

    id added

--- mock/customer/CustomerListMock.tsx
+++ mock/customer/CustomerListMock.tsx
@@ -1,64 +1,70 @@
 const customerDetails = [
   {
-    name: "Matheen",
+    id: 1,
+    name: 'Matheen',
     mobileNoList: 9945272789,
     noOfLoadsTaken: 0,
     systemMamul: 0,
     creditLimit: 5000,
     receivables: 0,
     workingCapitalDays: 7,
-    Status: "approved",
+    Status: 'approved'
   },
   {
-    name: "LRC LOGISTICS",
+    id: 2,
+    name: 'LRC LOGISTICS',
     mobileNoList: 9945248789,
     noOfLoadsTaken: 1,
     systemMamul: 0,
     creditLimit: 4000,
     receivables: 0,
     workingCapitalDays: 7,
-    Status: "approved",
+    Status: 'approved'
   },
   {
-    name: "Mana Transport Co",
+    id: 3,
+    name: 'Mana Transport Co',
     mobileNoList: 8885272789,
     noOfLoadsTaken: 0,
     systemMamul: 0,
     creditLimit: 1000,
     receivables: 0,
     workingCapitalDays: 5,
-    Status: "approved",
+    Status: 'approved'
   },
   {
-    name: "Kalaimagal transport",
+    id: 4,
+    name: 'Kalaimagal transport',
     mobileNoList: 9995272700,
     noOfLoadsTaken: 1,
     systemMamul: 0,
     creditLimit: 2000,
     receivables: 0,
     workingCapitalDays: 6,
-    Status: "approved",
+    Status: 'approved'
   },
   {
-    name: "Jai Balaji Logistics",
+    id: 5,
+    name: 'Jai Balaji Logistics',
     mobileNoList: 7745272788,
     noOfLoadsTaken: 0,
     systemMamul: 0,
     creditLimit: 3000,
     receivables: 0,
     workingCapitalDays: 5,
-    Status: "approved",
+    Status: 'approved'
   },
   {
-    name: "AKASH TRANSLINES",
+    id: 6,
+    name: 'AKASH TRANSLINES',
     mobileNoList: 9665272777,
     noOfLoadsTaken: 1,
     systemMamul: 0,
     creditLimit: 1000,
     receivables: 0,
     workingCapitalDays: 7,
-    Status: "approved",
-  },
-];
+    Status: 'approved'
+  }
+]
 
-export default customerDetails;
+export default customerDetails
