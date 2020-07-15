ed91a1d745f15c28baed5828e3d93f49cbc08de4
Shilpaveronica <Shilpaveronica@bitbucket.org>
2020-07-15 15:44:46 +0530

    create partner changes

--- src/pages/partners/create-partner.tsx
+++ src/pages/partners/create-partner.tsx
@@ -10,90 +10,90 @@ function PartnerProfile() {
                 <h4><b>Personal Details</b></h4><br />
                 <Row gutter={[12, 12]}>
                     <Col xs={24} sm={5}>
-                        <h4> <Row >
+                         <Row >
                             <Form.Item
                                 label="Partner Name (Should be RC name)"
                                 name="Partner Name (Should be RC name)"
-                                rules={[{ required: true, message: 'PartnerName' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="PartnerName" />
-                            </Form.Item> </Row> </h4>
+                            </Form.Item> </Row> 
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4>  <Row >
+                        <Row >
                             <Form.Item
                                 label="Conatct Person"
                                 name="Conatct Person"
-                                rules={[{ required: true, message: 'Contact Person' }]}
+                                rules={[{ required: true}]}
                             >
                                 <Input placeholder="Contact Person" />
-                            </Form.Item> </Row> </h4>
+                            </Form.Item> </Row> 
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4>  <Row >
+                        <Row >
                             <Form.Item
                                 label="Phone Number"
                                 name="Phone Number"
-                                rules={[{ required: true, message: 'Phone Number' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Phone Number" />
-                            </Form.Item> </Row> </h4>
+                            </Form.Item> </Row> 
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4>  <Row >
+                        <Row >
                             <Form.Item
                                 label="Email Address"
                                 name="Email Address"
-                                rules={[{ required: true, message: 'Email Address' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Email Address" />
-                            </Form.Item> </Row></h4>
+                            </Form.Item> </Row>
 
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4> <Row >
+                       <Row >
                             <Form.Item
                                 label="Pan Number"
                                 name="Pan Number"
-                                rules={[{ required: true, message: 'Pan Number' }]}
+                                rules={[{ required: true}]}
                             >
                                 <Input placeholder="Pan Number" />
-                            </Form.Item> </Row></h4>
+                            </Form.Item> </Row>
 
                     </Col>
                 </Row>
                 <Row gutter={[12, 12]}>
 
                     <Col xs={24} sm={{ span: 3 }} >
-                        <h5><b>Auto Final Payment</b></h5><br />
+                        <h4><b>Auto Final Payment</b></h4><br />
                         <RadioGroup >
                             <Radio value={1}>Yes</Radio>
                             <Radio value={0}>No</Radio>
                         </RadioGroup>
                     </Col>
-                    <Col xs={24} sm={2}>
+                    <Col xs={24} sm={3}>
 
                         <Row >
                             <Form.Item
                                 label="Cibil Score"
                                 name="Cibil Score"
-                                rules={[{ required: true, message: 'Cibil Score' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Cibil Score " />
                             </Form.Item> </Row>
 
                     </Col>
-                    <Col xs={24} sm={2}>
+                    <Col xs={24} sm={3}>
                         <Row >
                             <Form.Item
                                 label="Building Number"
                                 name="Building Number"
-                                rules={[{ required: true, message: 'Building Number' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Building Number" />
                             </Form.Item> </Row>
                     </Col>
-                    <Col xs={24} sm={3}>
+                    <Col xs={24} sm={4}>
                         <Row >
                             <Form.Item
                                 label="Address"
@@ -103,13 +103,12 @@ function PartnerProfile() {
                                 <Input placeholder="Address" />
                             </Form.Item> </Row>
                     </Col>
-                    <Col xs={24} sm={{ span: 4, offset: 10 }} >
+                    <Col xs={24} sm={{ span: 4, offset: 7 }} >
                         <Row >
                             <Form.Item
                                 label="Zip Code"
                                 name="Zip Code"
-
-                                rules={[{ required: true, message: 'Zip Code' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Zip Code" />
                             </Form.Item> </Row>
@@ -123,15 +122,15 @@ function PartnerProfile() {
                 <h4><b>Bank Details</b></h4><br />
                 <Row gutter={[12, 12]}>
                     <Col xs={24} sm={4}>
-                        <h4>
+                       
                             <Row >
                                 <Form.Item
                                     label="Account Holder Name"
                                     name="Account Holder Name"
-                                    rules={[{ required: true, message: 'Account Holder Name' }]}
+                                    rules={[{ required: true }]}
                                 >
                                     <Input placeholder="Address" />
-                                </Form.Item> </Row></h4>
+                                </Form.Item> </Row>
                     </Col>
                     <Col xs={24} sm={4}>
 
@@ -139,7 +138,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="Account No"
                                 name="Account No"
-                                rules={[{ required: true, message: 'Account No' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Account No" />
                             </Form.Item> </Row>
@@ -149,7 +148,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="Re-enter Account No"
                                 name="Re-enter Account No"
-                                rules={[{ required: true, message: 'Re-enter Account No' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="Confirm Account No" />
                             </Form.Item> </Row>
@@ -162,25 +161,35 @@ function PartnerProfile() {
                             <Form.Item
                                 label="IFSC Code"
                                 name="IFSC Code"
-                                rules={[{ required: true, message: 'IFSC Code' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input placeholder="IFSC Code" />
                             </Form.Item> </Row>
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4> <Row>Bank Name</Row></h4>
-                        <Row><Input placeholder="Bank Name" disabled /></Row>
+                    <Row >
+                            <Form.Item
+                                label="Bank Name"
+                                rules={[{ required: true}]}
+                            >
+                                <Input placeholder="Bank Name"/>
+                            </Form.Item> </Row>
 
                     </Col>
                     <Col xs={24} sm={4}>
-                        <h4> <Row>Branch Name</Row></h4>
-                        <Row><Input placeholder="Branch Name" disabled /></Row>
+                    <Row >
+                            <Form.Item
+                                label="Branch Name"
+                                rules={[{ required: true}]}
+                            >
+                                <Input placeholder="Branch Name"/>
+                            </Form.Item> </Row>
                     </Col>
                 </Row>
             </Card>
             <br />
             <Card>
-                <h5><b>FR8 Details</b></h5><br />
+                <h4><b>FR8 Details</b></h4><br />
                 <Row gutter={[12, 12]}>
 
                     <Col xs={24} sm={5}>
@@ -189,7 +198,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="Advance Percentage"
                                 name="Advance Percentage"
-                                rules={[{ required: true, message: 'Advance Percentage' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input />
                             </Form.Item> </Row>
@@ -200,7 +209,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="Region"
                                 name="Region"
-                                rules={[{ required: true, message: 'Region' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input />
                             </Form.Item> </Row>
@@ -211,7 +220,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="City"
                                 name="City"
-                                rules={[{ required: true, message: 'City' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input />
                             </Form.Item> </Row>
@@ -222,7 +231,7 @@ function PartnerProfile() {
                             <Form.Item
                                 label="On Boarded By"
                                 name="On Boarded By"
-                                rules={[{ required: true, message: 'On Boarded By' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input />
                             </Form.Item> </Row>
@@ -232,8 +241,7 @@ function PartnerProfile() {
                         <Row >
                             <Form.Item
                                 label="Lane Manager"
-
-                                rules={[{ required: true, message: 'Lane Manager' }]}
+                                rules={[{ required: true }]}
                             >
                                 <Input />
                             </Form.Item> </Row>
