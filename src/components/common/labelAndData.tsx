c7f393930151d83ac35bbbc6a81cd9f369e59418
Karthikeyan <>
2020-07-21 18:35:48 +0530

    lableAndData comp fix

--- src/components/common/labelAndData.tsx
+++ src/components/common/labelAndData.tsx
@@ -4,8 +4,8 @@ const LabelAndData = (props) => {
   const { label, colSpan, data } = props
   return (
     <Col xs={24} sm={colSpan || 12}>
-      <p className='mb0 b'>{label}</p>
-      {data}
+      <div>{label}</div>
+      <div>{data}</div>
     </Col>
   )
 }
