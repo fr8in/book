334b879e6ed61680ea4642ceb3842f9ee2a0518f
Karthikeyan <>
2020-07-22 01:16:27 +0530

    truck detail page ui fix

---
+++ mock/trucks/deviceHistory.js
@@ -0,0 +1,58 @@
+const deviceHistory = [
+  {
+    comment: [],
+    currentCity: 'Nagpur',
+    deviceStatusType: { id: 3, statusName: 'Intransit to Destination', __typename: 'DeviceStatusType' },
+    endDate: null,
+    gpsTypeId: 4,
+    id: 371339,
+    startDate: '2020-07-21T15:50:12.000+0000',
+    statusId: 3,
+    tripId: 192998
+  },
+  {
+    comment: [],
+    currentCity: 'Nagpur',
+    deviceStatusType: { id: 2, statusName: 'Waiting for Loading' },
+    endDate: null,
+    gpsTypeId: 4,
+    id: 371338,
+    startDate: '2020-07-21T07:49:50.000+0000',
+    statusId: 2,
+    tripId: 192998
+  },
+  {
+    comment: [],
+    currentCity: 'Nagpur',
+    deviceStatusType: { id: 1, statusName: 'Confirmed' },
+    endDate: null,
+    gpsTypeId: 4,
+    id: 371335,
+    startDate: '2020-07-21T15:41:44.000+0000',
+    statusId: 1,
+    tripId: 192998
+  },
+  {
+    comment: [
+      {
+        id: 708746,
+        comment: 'New Truck Attached',
+        createdBy: 'swati.shriniwas@fr8.in',
+        createdon: '2020-07-21T15:07:14.000+0000'
+      }
+    ],
+    currentCity: 'Nagpur',
+    deviceStatusType: {
+      id: 5,
+      statusName: 'Waiting for Load'
+    },
+    endDate: null,
+    gpsTypeId: 4,
+    id: 371313,
+    startDate: '2020-07-21T15:07:14.000+0000',
+    statusId: 5,
+    tripId: 192998
+  }
+]
+
+export { deviceHistory }
--- src/components/trucks/trucks.tsx
+++ src/components/trucks/trucks.tsx
@@ -1,35 +1,52 @@
-import React, {useState} from 'react'
-import trucks from '../../../mock/trucks/trucks'
-import { Table , Button} from 'antd'
+import { useState } from 'react'
+import { Table } from 'antd'
 import Link from 'next/link'
-import EditModal from '../../components/trucks/editModal'
-import PhoneModal from '../partners/phoneModal'
+import { EditTwoTone } from '@ant-design/icons'
+import trucks from '../../../mock/trucks/trucks'
+import CreateBreakdown from '../../components/trucks/createBreakdown'
+import PartnerUsers from '../partners/partnerUsers'
 
-const rowSelection = {
-  onChange: (selectedRowKeys, selectedRows) => {
-  },
-  getCheckboxProps: record => ({
-    disabled: record.name === 'Disabled User', 
-    name: record.name,
-  }),
-};
-
-
+const statusList = [
+  { value: 1, text: 'Ordered' },
+  { value: 2, text: 'Assigned' },
+  { value: 3, text: 'Confirmed' },
+  { value: 4, text: 'Waiting for Loading' },
+  { value: 5, text: 'Intransit to Destination' },
+  { value: 6, text: 'Deactivated' },
+  { value: 7, text: 'Intransit halting' }
+]
 
 const Trucks = () => {
-  const [selectionType,setSelectionType] = useState('checkbox');
+  const usersInitial = { users: [], name: '', visible: false }
+  const [users, setUsers] = useState(usersInitial)
 
-  const statusList = [
-    { value: 1, text: 'Ordered' },
-    { value: 2, text: 'Assigned' },
-    { value: 3, text: 'Confirmed' },
-    { value: 4, text: 'Waiting for Loading' },
-    { value: 5, text: 'Intransit to Destination' },
-    { value: 6, text: 'Deactivated' },
-    { value: 7, text: 'Intransit halting' }
-  ]
+  const initial = { record: null, title: '', visible: false }
+  const [availability, setAvailability] = useState(initial)
 
-  
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+    },
+    getCheckboxProps: record => ({
+      disabled: record.name === 'Disabled User',
+      name: record.name
+    })
+  }
+
+  const usersClose = () => {
+    setUsers(usersInitial)
+  }
+
+  const showUsers = (record) => {
+    setUsers({ ...users, users: record.users, name: record.partner, visible: true })
+  }
+
+  const breakdownClose = () => {
+    setAvailability(initial)
+  }
+
+  const showBreadown = (record) => {
+    setAvailability({ ...record, record: record, title: 'Breakdown', visible: true })
+  }
 
   const columns = [
     {
@@ -42,7 +59,6 @@ const Trucks = () => {
           </Link>
         )
       }
-
     },
     {
       title: 'Trip Id',
@@ -54,12 +70,10 @@ const Trucks = () => {
           </Link>
         )
       }
-
     },
     {
       title: 'Trip',
-      dataIndex: 'trip',
-
+      dataIndex: 'trip'
     },
     {
       title: 'Partner',
@@ -71,50 +85,58 @@ const Trucks = () => {
           </Link>
         )
       }
-
     },
     {
       title: 'Phone No',
       dataIndex: 'phoneNo',
-      render: () => {
+      render: (text, record) => {
         return (
-         <PhoneModal/>
+          <span className='link' onClick={() => showUsers(record)}>{text}</span>
         )
       }
-      
     },
     {
       title: 'Status',
       dataIndex: 'status',
-      filters : statusList
+      filters: statusList
     },
     {
-      title: 'city',
-      dataIndex: 'city',
+      title: 'City',
+      dataIndex: 'city'
     },
     {
       title: '',
-      render: () => {
-        return (
-         <EditModal/>
-        )
-      }
-      
-    }]
-
+      render: (text, record) => <EditTwoTone onClick={() => showBreadown(record)} />
+    }
+  ]
   return (
-    
-    <Table
-    rowSelection={{
-      ...rowSelection,
-    }}
-      columns={columns}
-      dataSource={trucks}
-      rowKey={record => record.id}
-      size='small'
-      scroll={{ x: 1156 }}
-      pagination={false}
-    />
+    <>
+      <Table
+        rowSelection={{
+          ...rowSelection
+        }}
+        columns={columns}
+        dataSource={trucks}
+        rowKey={record => record.id}
+        size='small'
+        scroll={{ x: 1156 }}
+        pagination={false}
+      />
+      {users.visible &&
+        <PartnerUsers
+          visible={users.visible}
+          data={users.users}
+          onHide={usersClose}
+          name={users.name}
+        />}
+      {availability.visible &&
+        <CreateBreakdown
+          visible={availability.visible}
+          data={availability.record}
+          onHide={breakdownClose}
+          title={availability.title}
+        />}
+    </>
   )
 }
 
