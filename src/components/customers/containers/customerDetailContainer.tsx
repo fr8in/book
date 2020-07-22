f8bf25fd395e9cb4b5aa1cb46ef3e77e6f3ef436
Karthikeyan <>
2020-07-22 09:08:22 +0530

    backend connected

--- src/components/customers/containers/customerDetailContainer.tsx
+++ src/components/customers/containers/customerDetailContainer.tsx
@@ -38,10 +38,10 @@ const CustomerDetailContainer = (props) => {
     }
   )
 
-  // if (loading) return <div>Loading...</div>
-  // const { customer } = data
-  // const customerInfo = customer[0] ? customer[0] : { name: 'ID does not exist' }
-  const customerInfo = {}
+  if (loading) return <div>Loading...</div>
+  const { customer } = data
+  const customerInfo = customer[0] ? customer[0] : { name: 'ID does not exist' }
+  // const customerInfo = {}
 
   return (
     <Row>
