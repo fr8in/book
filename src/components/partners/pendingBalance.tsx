29fec401a679086daa6c14825c36a8e1f3b94095
BalajiA22 <balaji.a@fr8.in>
2020-07-22 21:50:38 +0530

    onholdTrips

--- src/components/partners/pendingBalance.tsx
+++ src/components/partners/pendingBalance.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Card } from 'antd'
 import LabelData from '../../components/common/labelData'
+import useShowHide from '../../hooks/useShowHide'
+import OnHoldTrips from '../trips/onholdTrips'
 
 const PendingBalance = (props) => {
+  const initial = { onhold: false }
+  const { visible, onShow, onHide } = useShowHide(initial)
   return (
     <Card size='small'>
       <LabelData
@@ -12,12 +16,13 @@ const PendingBalance = (props) => {
       />
       <LabelData
         label='OnHold'
-        value={<h4 className='link u'>{props.onhold || 0}</h4>}
+        value={<h4 className='link u' onClick={() => onShow('onhold')}>{props.onhold || 0}</h4>}
       />
       <LabelData
         label='Cleared'
         value={props.cleared}
       />
+      {visible.onhold && <OnHoldTrips visible={visible.onhold} onHide={() => onHide('onhold')} />}
     </Card>
   )
 }
