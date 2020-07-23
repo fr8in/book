a2b53e26ff905e65d68a51f768e002ce784db85b
Shilpaveronica <Shilpaveronica@bitbucket.org>
2020-07-23 21:17:27 +0530

    partnersList data

--- src/pages/partners/index.tsx
+++ src/pages/partners/index.tsx
@@ -1,5 +1,5 @@
 import PageLayout from '../../components/layout/pageLayout'
-import PartnerList from '../../components/partners/container/partnerContainer'
+import PartnerList from '../../components/partners/container/partnersContainer'
 
 
 const Partner = () => {
