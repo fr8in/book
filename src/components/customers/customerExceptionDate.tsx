de7f97aa614e5ada76bc7e675b8c6048ea434a0b
Karthikeyan <>
2020-07-28 18:56:05 +0530

    exception date mutaion in-progress

--- src/components/customers/customerExceptionDate.tsx
+++ src/components/customers/customerExceptionDate.tsx
@@ -1,12 +1,59 @@
+import { useState } from 'react'
+import { message, DatePicker } from 'antd'
+import { CloseCircleOutlined, EditOutlined } from '@ant-design/icons'
 import moment from 'moment'
+import { UPDATE_CUSTOMER_EXCEPTION_MUTATION } from './containers/query/updateCustomerExceptionMutation'
+import { useMutation } from '@apollo/client'
 
 const CustomerExceptionDate = (props) => {
   const { exceptionDate, cardcode } = props
+
+  const [edit, setEdit] = useState(false)
+
+  const [updateCustomerException] = useMutation(
+    UPDATE_CUSTOMER_EXCEPTION_MUTATION,
+    {
+      onError (error) { message.error(error.toString()) }
+    }
+  )
+  const onChange = (date, dateString) => {
+    console.log(dateString)
+    updateCustomerException({
+      variables: {
+        cardcode,
+        exception_date: dateString.toString()
+      }
+    })
+    setEdit(false)
+  }
+
+  const handleShow = () => {
+    setEdit(true)
+  }
+  const handleClose = () => {
+    setEdit(false)
+  }
+
+  const dateFormat = 'YYYY-MM-DD'
   return (
     <div>
-      <label>
-        {exceptionDate ? moment(exceptionDate).format('DD-MM-YYYY') : '-'}
-      </label>
+      {!edit ? (
+        <label>
+          {exceptionDate ? moment(exceptionDate).format(dateFormat) : '-'}
+          <EditOutlined onClick={handleShow} />
+        </label>)
+        : (
+          <span>
+            <DatePicker
+              showToday={false}
+              placeholder='Exception Date'
+              disabled={false}
+              format={dateFormat}
+              value={moment(exceptionDate, dateFormat)}
+              onChange={onChange}
+            />
+            <CloseCircleOutlined onClick={handleClose} />
+          </span>)}
     </div>
   )
 }
