a36f8cdff4808f98d3244c356ce180b1d0ea0582
Karthikeyan <>
2020-07-29 16:53:01 +0530

    payment pending ui change

--- src/components/customers/containers/query/cutomerDetailSubscription.ts
+++ src/components/customers/containers/query/cutomerDetailSubscription.ts
@@ -7,18 +7,13 @@ export const CUSTOMER_DETAIL_SUBSCRIPTION = gql`
       cardcode
       name
       mobile
-      credit_limit
-      advance_percentage_id
       approved_by_id
-      created_at
       exception_date
       gst
       managed
       pan
       type_id
-      updated_at
       virtual_account
-      walletcode
       status {
         id
         value
