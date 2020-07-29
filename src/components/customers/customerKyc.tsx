7c778f75f8b71166a851b6f6015ef0022b7be0a4
Magaarajah <magaarajah.g@fr8.in>
2020-07-29 23:49:30 +0530

    popover and link added

--- src/components/customers/customerKyc.tsx
+++ src/components/customers/customerKyc.tsx
@@ -1,149 +1,157 @@
-import { Table, Button, Space, Tooltip } from 'antd'
+import { Table, Button, Space, Tooltip, Popconfirm } from "antd";
 import {
   CloseOutlined,
   CheckOutlined,
   EyeOutlined,
-  UploadOutlined
-} from '@ant-design/icons'
-import useShowHideWithRecord from '../../hooks/useShowHideWithRecord'
-import Link from 'next/link'
-import BranchCreation from '../customers/branchCreation'
+  UploadOutlined,
+} from "@ant-design/icons";
+import useShowHideWithRecord from "../../hooks/useShowHideWithRecord";
+import Link from "next/link";
+import BranchCreation from "../customers/branchCreation";
 
 const CustomerKyc = (props) => {
-  const { customers, status, statusId } = props
+  const { customers, status, statusId } = props;
   const initial = {
     visible: false,
     data: [],
     title: null,
     createBranchVisible: false,
-    createBranchData: []
-  }
-  const { object, handleHide, handleShow } = useShowHideWithRecord(initial)
+    createBranchData: [],
+  };
+  const { object, handleHide, handleShow } = useShowHideWithRecord(initial);
 
   const newCustomer = [
     {
-      title: 'User Name',
-      width: '12%',
+      title: "User Name",
+      width: "12%",
       render: (text, record) => {
-        const user = record.customerUsers[0] && record.customerUsers[0].name
+        const user = record.customerUsers[0] && record.customerUsers[0].name;
         return user && user.length > 14 ? (
           <Tooltip title={user}>
-            <span> {user.slice(0, 14) + '...'}</span>
+            <span> {user.slice(0, 14) + "..."}</span>
           </Tooltip>
         ) : (
           user
-        )
-      }
+        );
+      },
     },
     {
-      title: 'Company Name',
-      dataIndex: 'name',
-      width: '12%',
+      title: "Company Name",
+      dataIndex: "name",
+      width: "12%",
       render: (text, record) => {
         return (
-          <Link href='customers/[id]' as={`customers/${record.cardcode}`}>
+          <Link href="customers/[id]" as={`customers/${record.cardcode}`}>
             {text && text.length > 14 ? (
               <Tooltip title={text}>
-                <a>{text.slice(0, 14) + '...'}</a>
+                <a>{text.slice(0, 14) + "..."}</a>
               </Tooltip>
             ) : (
               <a>{text}</a>
             )}
           </Link>
-        )
-      }
+        );
+      },
     },
     {
-      title: 'Mobile No',
-      dataIndex: 'mobile',
-      width: '10%'
+      title: "Mobile No",
+      dataIndex: "mobile",
+      width: "10%",
     },
     {
-      title: 'Type',
-      dataIndex: 'type_id',
-      width: '10%'
+      title: "Type",
+      dataIndex: "type_id",
+      width: "10%",
     },
     {
-      title: 'Reg Date',
-      dataIndex: 'created_at',
-      width: '9%',
+      title: "Reg Date",
+      dataIndex: "created_at",
+      width: "9%",
       render: (text, record) => {
         return text && text.length > 10 ? (
           <Tooltip title={text}>
-            <span> {text.slice(0, 10) + '..'}</span>
+            <span> {text.slice(0, 10) + ".."}</span>
           </Tooltip>
         ) : (
           text
-        )
-      }
+        );
+      },
     },
     {
-      title: 'PAN',
-      dataIndex: 'pan',
-      width: '9%'
+      title: "PAN",
+      dataIndex: "pan",
+      width: "9%",
     },
     {
-      title: 'De. Mamul',
-      dataIndex: 'mamul',
-      width: '8%'
+      title: "De. Mamul",
+      dataIndex: "mamul",
+      width: "8%",
     },
     {
-      title: 'Adv %',
-      width: '6%',
+      title: "Adv %",
+      width: "6%",
       render: (text, record) =>
-        record.advancePercentage && record.advancePercentage.value
+        record.advancePercentage && record.advancePercentage.value,
     },
     {
-      title: 'Status',
+      title: "Status",
       render: (text, record) => record.status && record.status.value,
-      width: '14%',
+      width: "14%",
       defaultFilteredValue: statusId,
-      filters: status
+      filters: status,
     },
     {
-      title: 'Action',
-      width: '10%',
+      title: "Action",
+      width: "10%",
       render: (text, record) => {
         return (
           <Space>
             {record.panNo ? (
               <Button
-                type='primary'
-                size='small'
-                shape='circle'
+                type="primary"
+                size="small"
+                shape="circle"
                 icon={<EyeOutlined />}
-                onClick={() => console.log('View')}
+                onClick={() => console.log("View")}
               />
             ) : (
               <Button
-                size='small'
-                shape='circle'
+                size="small"
+                shape="circle"
                 icon={<UploadOutlined />}
-                onClick={() => console.log('Upload')}
+                onClick={() => console.log("Upload")}
               />
             )}
             <Button
-              type='primary'
-              size='small'
-              shape='circle'
-              className='btn-success'
+              type="primary"
+              size="small"
+              shape="circle"
+              className="btn-success"
               icon={<CheckOutlined />}
               onClick={() =>
-                handleShow('createBranchVisible', null, null, record)}
+                handleShow("createBranchVisible", null, null, record)
+              }
             />
-            <Button
-              type='primary'
-              size='small'
-              shape='circle'
-              danger
-              icon={<CloseOutlined />}
-              onClick={() => handleShow(null, null, null, null)}
-            />
+            <Popconfirm
+              title="Are you sure want to Reject the Customer?"
+              okText="Yes"
+              cancelText="No"
+              onConfirm={() => console.log("Rejected!")}
+            >
+              <Button
+                type="primary"
+                size="small"
+                shape="circle"
+                danger
+                icon={<CloseOutlined />}
+                onClick={() => handleShow(null, null, null, null)}
+              />
+            </Popconfirm>
           </Space>
-        )
-      }
-    }
-  ]
+        );
+      },
+    },
+  ];
 
   return (
     <>
@@ -151,7 +159,7 @@ const CustomerKyc = (props) => {
         columns={newCustomer}
         dataSource={customers}
         rowKey={(record) => record.cardcode}
-        size='small'
+        size="small"
         scroll={{ x: 960, y: 400 }}
         pagination={false}
       />
@@ -163,7 +171,7 @@ const CustomerKyc = (props) => {
         />
       )}
     </>
-  )
-}
+  );
+};
 
-export default CustomerKyc
+export default CustomerKyc;
