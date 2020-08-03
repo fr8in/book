1f2da3f84022d7b22cb18667731fe898bdc443c2
Karthikeyan <>
2020-08-03 14:04:48 +0530

    graphql url updated

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,39 +1,39 @@
 import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client'
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-//import WebSocket from 'ws';
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+// import WebSocket from 'ws';
 
-const isBrowser = typeof window !== 'undefined';
+const isBrowser = typeof window !== 'undefined'
 const wsLink = isBrowser ? new WebSocketLink({
-    uri: 'ws://prodcore.southeastasia.azurecontainer.io/v1/graphql',
-    options: {
-        reconnect: true
-    },
-    //webSocketImpl: WebSocket
+  uri: 'ws://dev-fr8.southeastasia.azurecontainer.io/v1/graphql',
+  options: {
+    reconnect: true
+  }
+  // webSocketImpl: WebSocket
 }) : null
 console.log(' process.browser: ', process.browser)
 const httpLink = new HttpLink({
-    uri: process.env.CORE || 'http://prodcore.southeastasia.azurecontainer.io/v1/graphql',
-    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+  uri: process.env.CORE || 'http://dev-fr8.southeastasia.azurecontainer.io/v1/graphql',
+  credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
 })
 const link = isBrowser ? split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
             definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
+    )
+  },
+  wsLink,
+  httpLink
 ) : httpLink
 
-export default function createApolloClient(initialState, ctx) {
-    // The `ctx` (NextPageContext) will only be present on the server.
-    // use it to extract auth headers (ctx.req) or similar.
-    return new ApolloClient({
-        ssrMode: Boolean(ctx),
-        link,
-        cache: new InMemoryCache().restore(initialState),
-    })
+export default function createApolloClient (initialState, ctx) {
+  // The `ctx` (NextPageContext) will only be present on the server.
+  // use it to extract auth headers (ctx.req) or similar.
+  return new ApolloClient({
+    ssrMode: Boolean(ctx),
+    link,
+    cache: new InMemoryCache().restore(initialState)
+  })
 }
