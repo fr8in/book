041f432cf1ff81f5eb0e370e2880d7b010e4b3e1
Devavarshni_2418 <devavarshni.mb@fr8.in>
2020-08-07 18:50:52 +0530

    truckinfo

--- src/components/trucks/truckInfo.tsx
+++ src/components/trucks/truckInfo.tsx
@@ -1,25 +1,37 @@
 
-import { Row, Col, Form, Input } from 'antd'
+import { Row, Col, Form, Input, Button } from 'antd'
 
-const TruckInfo = () => {
+
+const TruckInfo = (props) => {
+  const {truck_info} = props
+
+  console.log('truck_info',truck_info.length )
   return (
+    
     <div>
-      <Form layout='vertical'>
+      <Form layout='vertical'  >
         <Row gutter={10}>
           <Col span={6}>
+          
             <Form.Item
               label='Length(Ft)'
               name='Length(Ft)'
-              rules={[{ required: true, message: 'Length(Ft) is required field' }]}
+              rules={[{ required: true, message: 'Length(Ft) is required field' }]}  
+              initialValue={truck_info.length }
+              
             >
-              <Input placeholder='Length(Ft)' type='number' disabled={false} />
+              
+              <Input placeholder='Length(Ft)' type='number' disabled={false}  />
+          
             </Form.Item>
+            
           </Col>
           <Col span={6}>
             <Form.Item
               label='Breadth(Ft)'
               name='Breadth(Ft)'
               rules={[{ required: true, message: 'Breadth(Ft) is required field' }]}
+              initialValue={truck_info.breadth }
             >
               <Input placeholder='Breadth(Ft)' type='number' disabled={false} />
             </Form.Item>
@@ -29,9 +41,15 @@ const TruckInfo = () => {
               label='Height(Ft)'
               name='Height(Ft)'
               rules={[{ required: true, message: 'Height(Ft) is required field' }]}
+              initialValue={truck_info.height }
             >
               <Input placeholder='Height(Ft)' type='number' disabled={false} />
             </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label='save' name='save' className='hideLabel'>
+          <Button  type="primary"> Save </Button>
+          </Form.Item>
           </Col>
           <Col span={6}>
             <Form.Item
