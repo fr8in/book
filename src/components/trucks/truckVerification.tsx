aae9921de3e73e681dc3800bea28efba80029e4c
Devavarshni_2418 <devavarshni.mb@fr8.in>
2020-08-08 18:48:13 +0530

    truckVerification

--- src/components/trucks/truckVerification.tsx
+++ src/components/trucks/truckVerification.tsx
@@ -1,4 +1,5 @@
-import { Table, Button, Space } from "antd";
+import { useState } from 'react'
+import { Table, Button, Space ,Pagination} from "antd";
 import mock from "../../../mock/partner/sourcingMock";
 import Link from "next/link";
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
@@ -6,27 +7,80 @@ import useShowHide from "../../hooks/useShowHide";
 import TruckReject from "../../components/trucks/truckReject";
 import TruckActivation from "../trucks/truckActivation";
 import useShowHideWithRecord from "../../hooks/useShowHideWithRecord";
+import { gql, useQuery } from '@apollo/client'
 
+
+const TRUCKS_QUERY = gql`
+query trucks {
+    truck( where: {truck_status: {name: {_in: ["Breakdown","Deactivated"],}}}) {
+      truck_no
+      truck_status {
+        id
+        name
+      }
+      partner {
+        id
+        cardcode
+        name
+      }
+    }
+    truck_aggregate(where:{truck_status: {name: {_in: ["Breakdown","Deactivated"],}}}){
+      aggregate{
+        count
+     }
+    }
+  }
+  
+`
 const status = [
   { value: 1, text: "Verification Pending" },
   { value: 2, text: "Rejected" },
 ];
-const TruckVerification = () => {
+
+const TruckVerification = (props) => {
+ 
+  
+  
+  
+
+
   const initial = {
     reject: false,
     truckActivationVisible: false,
     truckActivationData: [],
   };
+
   const { visible, onShow, onHide } = useShowHide(initial);
   const { object, handleHide, handleShow } = useShowHideWithRecord(initial);
+
+
+  const { loading, error, data } = useQuery(TRUCKS_QUERY, {
+    
+    notifyOnNetworkStatusChange: true
+  })
+
+  console.log('TrucksVerification error', error)
+
+  var truck = []
+ 
+  
+  if (!loading) {
+  truck = data && data.truck
+  
+  }
+
+ 
+ 
+
+  
   const columnsCurrent = [
     {
       title: "Truck No",
-      dataIndex: "truckNo",
+      dataIndex: "truck_no",
       render: (text, record) => {
         return (
-          <Link href="trucks/[id]" as={`trucks/${record.id}`}>
-            <a>{text}</a>
+          <Link href="trucks/[id]" as={`trucks/${record.truck_no}`}>
+            <a>{record.truck_no}</a>
           </Link>
         );
       },
@@ -37,8 +91,8 @@ const TruckVerification = () => {
       dataIndex: "code",
       render: (text, record) => {
         return (
-          <Link href="partners/[id]" as={`partners/${record.id}`}>
-            <a>{text}</a>
+          <Link href="partners/[id]" as={`partners/${record.partner && record.partner.cardcode}`}>
+            <a>{record.partner && record.partner.cardcode}</a>
           </Link>
         );
       },
@@ -48,12 +102,18 @@ const TruckVerification = () => {
       title: "Partner",
       dataIndex: "partner",
       width: "18%",
+      render: (text, record) => {
+        return record.partner && record.partner.name;
+      },
     },
     {
       title: "Truck Status",
       dataIndex: "status",
       filters: status,
       width: "17%",
+      render: (text, record) => {
+        return record.truck_status && record.truck_status.name;
+      },
     },
     {
       title: "Action",
@@ -98,13 +158,15 @@ const TruckVerification = () => {
     <>
       <Table
         columns={columnsCurrent}
-        dataSource={mock}
+        dataSource={truck}
         rowKey={(record) => record.id}
         size="middle"
         scroll={{ x: 1150 }}
         pagination={false}
         className="withAction"
+       
       />
+      
       {visible.reject && (
         <TruckReject visible={visible.reject} onHide={onHide} />
       )}
