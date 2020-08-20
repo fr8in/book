443e4bf353e439cb5136e90d1727bcab9b62a6e8
Karthikeyan <>
2020-08-21 00:06:01 +0530

    PO_in-progress

--- src/components/branches/fr8EmpolyeeList.tsx
+++ src/components/branches/fr8EmpolyeeList.tsx
@@ -1,8 +1,8 @@
-import { Modal, Select,message } from 'antd'
-import { gql, useQuery,useMutation } from '@apollo/client'
+import { Modal, Select, message } from 'antd'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import { useState } from 'react'
 
-const PARTNERS_SUBSCRIPTION = gql`
+const EMP_LIST = gql`
 query create_partner{
   employee{
     id
@@ -21,12 +21,12 @@ mutation update_owner($id:Int,$onboarded_by_id:Int) {
 `
 
 const EmployeeList = (props) => {
-  const { visible, onHide,partner } = props
-const onboarded_by = partner && partner.onboarded_by && partner.onboarded_by.email
-const [employees, setEmployees] = useState('')
-console.log('onboarded_by',onboarded_by)
+  const { visible, onHide, partner } = props
+  const onboarded_by = partner && partner.onboarded_by && partner.onboarded_by.email
+  const [employees, setEmployees] = useState('')
+  console.log('onboarded_by', onboarded_by)
   const { loading, error, data } = useQuery(
-    PARTNERS_SUBSCRIPTION,
+    EMP_LIST,
     {
       fetchPolicy: 'cache-and-network',
       notifyOnNetworkStatusChange: true
@@ -40,33 +40,33 @@ console.log('onboarded_by',onboarded_by)
       onCompleted () { message.success('Updated!!') }
     }
   )
- 
-  var employee = [];
+
+  var employee = []
   if (!loading) {
-     employee = data && data.employee
+    employee = data && data.employee
   }
   const employeeList = employee.map((data) => {
     return { value: data.id, label: data.email }
   })
-  
+
   const employeeChange = (value) => {
     setEmployees(value)
   }
   const onSubmit = () => {
     updateOwner({
-      variables:{
-        id:partner.id,
+      variables: {
+        id: partner.id,
         onboarded_by_id: employees
       }
-    }) 
+    })
   }
- 
+
   return (
     <Modal
       visible={visible}
       onOk={onSubmit}
       onCancel={onHide}
-    >   
+    >
       <Select value={employees} onChange={employeeChange} style={{ width: 300 }} options={employeeList} />
     </Modal>
   )
