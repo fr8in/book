d52be3c2538d0f8d782080429069db6a5ca3cbd7
Karthikeyan <>
2020-08-29 13:38:02 +0530

    payable payment code review done

--- src/components/trips/receivables.tsx
+++ src/components/trips/receivables.tsx
@@ -1,16 +1,13 @@
-import { Table, Row, Col, Tooltip } from 'antd'
-//  import data from '../../../mock/trip/chargesAndPayments'
+import { Table, Row, Col } from 'antd'
 import moment from 'moment'
 import _ from 'lodash'
+import Truncate from '../common/truncate'
 
 const Receivables = (props) => {
-  const {trip_pay} = props
-  
-  const receipts = _.sumBy(trip_pay.trip_receipts, 'amount');
-console.log('receipts',receipts)
+  const { trip_pay } = props
 
-const receivables = _.sumBy(trip_pay.trip_receivables, 'amount');
-console.log('receivables',receivables)
+  const receivables = _.sumBy(trip_pay.trip_receivables, 'amount').toFixed(2)
+  const receipts = _.sumBy(trip_pay.trip_receipts, 'amount').toFixed(2)
 
   const customerChargeColumns = [
     {
@@ -20,61 +17,42 @@ console.log('receivables',receivables)
     {
       title: <div className='text-right'>Amount</div>,
       dataIndex: 'amount',
-      render: (text, record) => {
-        return text
-          ? <div className='text-right'> {text ? text.toFixed(2) : 0}</div>
-          : <div className='text-right'>0</div>
-      }
+      className: 'text-right',
+      render: (text, record) => text ? text.toFixed(2) : 0
     }
   ]
-  const advanceColumn = [{
-    title: 'Mode',
-    dataIndex: 'mode',
-    width: '27%',
-    render: (text,record) => {
-      return (record.mode)
-    }
-  },
-  {
-    title: 'Date',
-    dataIndex: 'created_at',
-    width: '16%',
-    render: (text, record) => {
-      return (record.created_at) ? moment(record.created_at).format('DD MMM YYYY') : null
-    }
-  },
-  {
-      title: 'Remarks',
-      dataIndex: 'paymentComment',
-      key: 'paymentComment',
-      width: '37%',
-      render: (text, record) => {
-        const displayRecord =
-        record.comment.length > 35 ? (
-          <Tooltip title={record.comment}>
-            <span>{record.comment.slice(0, 28) + '...'}</span>
-          </Tooltip>
-        ) : (
-          record.comment
-        )
-        return displayRecord
-      }
+  const advanceColumn = [
+    {
+      title: 'Mode',
+      dataIndex: 'mode',
+      width: '27%',
+      render: (text, record) => text
     },
-  {
-    title: <div className='text-right'> Amount</div>,
-    dataIndex: 'amount',
-    width: '20%',
-    render: (text, record) => {
-      return text
-        ? <div className='text-right'> {text ? text.toFixed(2) : 0}</div>
-        : <div className='text-right'>0</div>
+    {
+      title: 'Date',
+      dataIndex: 'created_at',
+      width: '18%',
+      render: (text, record) => text ? moment(text).format('DD MMM YY') : null
+    },
+    {
+      title: 'Remarks',
+      dataIndex: 'comment',
+      key: 'comment',
+      width: '35%',
+      render: (text, record) => <Truncate data={text} length={20} />
+    },
+    {
+      title: <div className='text-right'>Amount</div>,
+      dataIndex: 'amount',
+      width: '20%',
+      className: 'text-right',
+      render: (text, record) => text ? text.toFixed(2) : 0
     }
-  }
   ]
 
   return (
     <>
-    <Row className='payableHead' gutter={6}>
+      <Row className='payableHead' gutter={6}>
         <Col xs={12}><b>Receivables</b></Col>
         <Col xs={12} className='text-right'>
           <b>{receivables}</b>
