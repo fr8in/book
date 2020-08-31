56e22233b1d6544c360b86ba9981281583fb6676
BalajiA22 <balaji.a@fr8.in>
2020-08-31 17:50:16 +0530

    cityBranch

--- src/components/branches/container/branchesContainer.tsx
+++ src/components/branches/container/branchesContainer.tsx
@@ -5,12 +5,40 @@ import AddBranch from '../addBranch'
 import { PlusCircleOutlined } from '@ant-design/icons'
 import { Tabs, Card, Button } from 'antd'
 import useShowHide from '../../../hooks/useShowHide'
+import { gql, useSubscription } from '@apollo/client'
+import get from 'lodash/get'
 
+const CITY_QUERY = gql`
+subscription{
+  city{
+    id
+    name
+    branch{
+      id
+      name
+    }
+  }
+}
+`
 const TabPane = Tabs.TabPane
 
 const BranchesContainer = () => {
   const initial = { showModal: false }
   const { visible, onShow, onHide } = useShowHide(initial)
+  const { loading, error, data } = useSubscription(
+    CITY_QUERY, 
+    {
+
+  })
+  console.log('error', error)
+  
+let _data = []
+if (!loading){
+     _data = data 
+}
+const citymapping = get(_data,'city',[])
+console.log('city',citymapping)
+
   return (
     <Card
       size='small'
@@ -35,7 +63,7 @@ const BranchesContainer = () => {
           <Employees />
         </TabPane>
         <TabPane tab='City Branch Mapping' key='3'>
-          <CityBranchMapping />
+          <CityBranchMapping citymapping={citymapping} />
         </TabPane>
       </Tabs>
       {visible.showModal && (
