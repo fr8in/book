74ea437e33c7cfaf098576247636cf848b1491c2
Devavarshni M B <devavarshni.mb@fr8.in>
2020-08-31 20:22:08 +0530

    phonenumberedit

---
+++ src/components/branches/employeeNumber.tsx
@@ -0,0 +1,44 @@
+import { gql, useMutation } from '@apollo/client'
+import { message } from 'antd'
+import EditableCell from '../common/editableCell'
+
+
+const UPDATE_EMPLOYEE_NO_MUTATION = gql`
+mutation insert_branch($id:Int!, $mobile:String) {
+    update_employee_by_pk(pk_columns:{id:$id}, _set:{mobileno:$mobile}){
+      id
+    }
+  }
+`
+const EmployeeNumber = (props) => {
+
+ const { id , label} = props
+
+ 
+
+  const [updateTruckNo] = useMutation(
+    UPDATE_EMPLOYEE_NO_MUTATION,
+    {
+      onError (error) { message.error(error.toString()) },
+      onCompleted () { message.success('Saved!!') }
+    }
+  )
+
+  const onSubmit = (value) => {
+   console.log('id',id,value)
+   updateTruckNo({
+     variables: {  id:id,
+      mobile:value
+     }
+   })
+  }
+
+  return (
+    <EditableCell
+   label={label}
+   onSubmit={onSubmit}
+    />
+  )
+}
+
+export default EmployeeNumber
