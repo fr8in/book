cef91b1d45b4a70ac3ee89bd82f7a2c3b1e3ba2f
BalajiA22 <balaji.a@fr8.in>
2020-09-02 22:14:38 +0530

    customeruser

--- src/components/customers/createCustomerUser.tsx
+++ src/components/customers/createCustomerUser.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import { Row, Col, Modal, Button, Input, Select, Form, message } from 'antd'
-import { useMutation, gql, useQuery } from '@apollo/client'
-import CitySelect from '../common/citySelect'
 
-const CUSTOMER_USERS_SUBSCRIPTION = gql`
-  query customer{
-    branch{
-      id
-      name
-    }
-  }
-`
+import { Row, Col, Modal, Button, Input, Form, message } from 'antd'
+import { useMutation, gql } from '@apollo/client'
+
 const INSERT_CUSTOMER_USERS_MUTATION = gql`
 mutation CustomeruserInsert($name:String,$mobile:String,$email:String,$customer_id:Int,) {
   insert_customer_user(
@@ -31,49 +22,20 @@ mutation CustomeruserInsert($name:String,$mobile:String,$email:String,$customer_
 
 const CreateCustomerUser = (props) => {
   const { visible, onHide, customer } = props
-  const initial = { city_id: null }
-  const [obj, setObj] = useState(initial)
-
-  const { loading, error, data } = useQuery(
-    CUSTOMER_USERS_SUBSCRIPTION,
-    {
-      fetchPolicy: 'cache-and-network',
-      notifyOnNetworkStatusChange: true
-    }
-  )
-  console.log('createCustomerUser error', error)
 
   const [insertcustomerUser] = useMutation(
     INSERT_CUSTOMER_USERS_MUTATION,
     {
-      onError (error) { message.error(error.toString()) },
-      onCompleted () {
+      onError(error) { message.error(error.toString()) },
+      onCompleted() {
         message.success('Updated!!')
-        setObj(initial)
       }
     }
   )
 
-  var branch = []
-  if (!loading) {
-    branch = data.branch
-  }
-
-  const branchList = branch.map((data) => {
-    return { value: data.id, label: data.name }
-  })
-
-  function handleChange (value) {
-    console.log(`selected ${value}`)
-  }
-  const onCityChange = (city_id) => {
-    setObj({ ...obj, city_id: city_id })
-  }
-
   const onAddUser = (form) => {
     insertcustomerUser({
       variables: {
-        city_id: obj.city_id,
         customer_id: customer,
         mobile: form.mobile,
         email: `${form.mobile}.customer@fr8.in`,
@@ -102,18 +64,6 @@ const CreateCustomerUser = (props) => {
             <Form.Item name='email'>
               <Input placeholder='E-Mail' />
             </Form.Item>
-            <Row gutter={6}>
-              <Col xs={12}>
-                <Form.Item>
-                  <Select defaultValue='User Branch' onChange={handleChange} options={branchList} />
-                </Form.Item>
-              </Col>
-              <Col xs={12}>
-                <CitySelect
-                  onChange={onCityChange}
-                />
-              </Col>
-            </Row>
             <Row justify='end'>
               <Form.Item>
                 <Button key='back' onClick={onHide}>Cancel</Button>
