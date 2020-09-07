b9a78e39f0cdbe61aba665726bbde16a92b33f5b
Devavarshni M B <devavarshni.mb@fr8.in>
2020-09-07 15:26:09 +0530

    driverupdate

--- src/components/trips/containers/query/tripDetailSubscription.ts
+++ src/components/trips/containers/query/tripDetailSubscription.ts
@@ -10,7 +10,7 @@ export const TRIP_DETAIL_SUBSCRIPTION = gql`
       status_at
       destination_in
       destination_out
-      driver
+     driver_id
       delay
       eta
       po_date
@@ -93,7 +93,7 @@ export const TRIP_DETAIL_SUBSCRIPTION = gql`
         mamul
         including_loading
         including_unloading
-        customer_advance_percentage
+        ##customer_advance_percentage
         ton
         is_price_per_ton
         price_per_ton
