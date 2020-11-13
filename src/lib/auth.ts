b05ceb0eba8fe3df50c051934b33783afecc6e1a
jay <jay@fr8.in>
2020-11-13 10:39:13 +0530

    server fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,20 +6,15 @@ import { message } from 'antd'
 import jwt from 'jsonwebtoken'
 import moment from 'moment'
 
+
 if (!firebase.apps.length) {
-  const { API_KEY,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_ID } = process.env
   firebase.initializeApp({
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_ID
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID
   })
 }
 
