88cc5ce4191420215adfc3e8289a8aec89c7d857
jsanjeyKumar@gmail.com <jsanjeykumr@gmail.com>
2020-12-29 18:05:20 +0530

    conflict fixed

--- src/components/partners/payables/container/payablesContainer.tsx
+++ src/components/partners/payables/container/payablesContainer.tsx
@@ -1,20 +1,18 @@
-
 import ICICIBankOutgoing from '../iciciBankOutgoing'
-import React, { useState, useContext } from 'react'
-import { Tabs, Space, Card, Button, DatePicker, message  } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons'
-import { gql, useMutation } from '@apollo/client'
-
+import React, {useContext, useState} from 'react'
+import {Button, Card, DatePicker, message, Space, Tabs} from 'antd'
+import {DownloadOutlined} from '@ant-design/icons'
+import {gql, useMutation} from '@apollo/client'
+import moment from 'moment'
 import isEmpty from 'lodash/isEmpty'
+import RelianceCashBack from '../reliancecashback/RelianceCashBack';
+import u from '../../../../lib/util'
+import isNil from 'lodash/isNil';
 import CashBack from '../cashBack'
 import CashBackButton from '../cashBackButton'
-import u from '../../../../lib/util'
 import userContext from '../../../../lib/userContaxt'
-import RelianceCashBack from '../reliancecashback/RelianceCashBack';
-import util from '../../../../lib/util'
-import isNil from 'lodash/isNil';
 
-const { RangePicker } = DatePicker
+const {RangePicker} = DatePicker
 
 const DATE_SELECT_MUTATION = gql`
 mutation icici_statement($start_date:String!,$end_date:String!) {
@@ -23,149 +21,121 @@ mutation icici_statement($start_date:String!,$end_date:String!) {
 
 const TabPane = Tabs.TabPane
 const PayablesContainer = () => {
-  const [dates, setDates] = useState([])
-  const [tabIndex, setTabIndex] = useState("0")
-  const [month, setMonth] = useState(null)
-  const [year, setYear] = useState(null)
-  const [tabIndex, setTabIndex] = useState('2')
-  const [month, setMonth] = useState(null)
-  const [year, setYear] = useState(null)
+    const [dates, setDates] = useState([])
+    const [tabIndex, setTabIndex] = useState("0")
+    const [month, setMonth] = useState(null)
+    const [year, setYear] = useState(null)
 
-  const handleMonthChange = (date, dateString) => {
-    const splittedDate = dateString.split("-")
-    setYear(parseInt(splittedDate[0]))
-    setMonth(parseInt(splittedDate[1]))
-  }
-
-  const disabledDate = (current) => {
-    if (!dates || dates.length === 0) {
-      return false
+    const handleMonthChange = (date, dateString) => {
+        const splittedDate = dateString.split("-")
+        setYear(parseInt(splittedDate[0]))
+        setMonth(parseInt(splittedDate[1]))
     }
-    const tooLate = dates[0] && current.diff(dates[0], 'days') > 30
-    const tooEarly = dates[1] && dates[1].diff(current, 'days') > 30
-    return ((tooEarly || tooLate))
-  }
 
-  const [icici_statement] = useMutation(
-    DATE_SELECT_MUTATION,
-    {
-      onError(error) { message.error(error.toString()) },
-      onCompleted(data) {
-        const url = data && data.icici_statement
-        window.open(url, 'icici_statement')
-        message.success('Updated!!')
-      }
-    }
-  )
-  const onConfirm = () => {
-    if (!isEmpty(dates)) {
-      icici_statement({
-        variables: {
-          start_date: dates[0].format('DD-MM-YYYY'),
-          end_date: dates[1].format('DD-MM-YYYY')
+    const disabledDate = (current) => {
+        if (!dates || dates.length === 0) {
+            return false
         }
-      })
-    } else {
-      message.error('Select Start date and End date!')
-    }
-  }
-
-  const getTabBarContent = () => {
-    if (tabIndex === '2') {
-      return <DatePicker onChange={handleMonthChange} picker="month" />
-    }
-    else {
-      return <Space>
-        <RangePicker
-          size='small'
-          format='DD-MM-YYYY'
-          disabledDate={(current) => disabledDate(current)}
-          onCalendarChange={(value) => {
-            setDates(value)
-          }}
-        />
-        <Button size='small'>
-          <DownloadOutlined onClick={() => onConfirm()} />
-        </Button>
-      </Space>
+        const tooLate = dates[0] && current.diff(dates[0], 'days') > 30
+        const tooEarly = dates[1] && dates[1].diff(current, 'days') > 30
+        return ((tooEarly || tooLate))
     }
 
-  }
-
-
-  return (
-    <Card size='small' className='card-body-0 border-top-blue'>
-      <Tabs
-        tabBarExtraContent={getTabBarContent()}
-        defaultActiveKey={tabIndex}
-        onChange={(e) => setTabIndex(e)}
-
-  const handleMonthChange = (date, dateString) => {
-    const splittedDate = dateString.split("-")
-    setYear(parseInt(splittedDate[0]))
-    setMonth(parseInt(splittedDate[1]))
-  }
-  const previousMonth = moment().subtract(1, 'months')
-
-  const handleCashBackDate = (date) => {
-    return moment().diff(date, 'months') > 1 || moment().diff(date, 'months') < 1
-  }
-
-  const context = useContext(userContext)
-  const { role } = u
-
-  const roles = [role.admin]
-  const access = u.is_roles(roles, context)
-
-
-  return (
-    <Card size='small' className='card-body-0 border-top-blue'>
-      <Tabs
-        tabBarExtraContent={
-          <Space>
-            {tabIndex === '0' ? <><RangePicker
-              size='small'
-              format='DD-MM-YYYY'
-              disabledDate={(current) => disabledDate(current)}
-              onCalendarChange={(value) => {
-                setDates(value)
-              }}
-            />
-              <Button size='small'>
-                <DownloadOutlined onClick={() => onConfirm()} />
-              </Button></> :
-              <>
-                {access && <>  <DatePicker
-                  disabledDate={(date) => handleCashBackDate(date)}
-                  onChange={handleMonthChange} picker="month" />
-                  <CashBackButton month={month}
-                    year={year} /></>}
-              </>
+    const [icici_statement] = useMutation(
+        DATE_SELECT_MUTATION,
+        {
+            onError(error) {
+                message.error(error.toString())
+            },
+            onCompleted(data) {
+                const url = data && data.icici_statement
+                window.open(url, 'icici_statement')
+                message.success('Updated!!')
             }
-          </Space>
         }
-        defaultActiveKey={tabIndex}
-        onChange={(e) => setTabIndex(e)}
-      >
-        <TabPane tab='ICICI Bank Outgoing' key={'0'}>
-          <ICICIBankOutgoing />
-        </TabPane>
+    )
+    const onConfirm = () => {
+        if (!isEmpty(dates)) {
+            icici_statement({
+                variables: {
+                    start_date: dates[0].format('DD-MM-YYYY'),
+                    end_date: dates[1].format('DD-MM-YYYY')
+                }
+            })
+        } else {
+            message.error('Select Start date and End date!')
+        }
+    }
 
-        <TabPane tab='Reliance' key={'2'}>
-        {isNil(month) || isNil(year) ?
-        <> <h4 align='center'>Reliance Fuel CashBack</h4></> :
-          <RelianceCashBack month={month} year={year} setMonth = {setMonth} setYear = {setYear}/> }
-        </TabPane>
+    const handleCashBackDate = (date) => {
+        return moment().diff(date, 'months') > 1 || moment().diff(date, 'months') < 1
+    }
+    const context = useContext(userContext)
+    const {role} = u
 
-        {access && <TabPane tab='Transaction Fee' key={'1'}>
-          <CashBack
-            month={month}
-            year={year}
-          />
-        </TabPane>}
-      </Tabs>
-    </Card >
-  )
+    const roles = [role.admin]
+    const access = u.is_roles(roles, context)
+
+    const fuelCashback_roles = [role.admin, role.accounts_manager, role.accounts]
+    const fuelCashback_access = u.is_roles(fuelCashback_roles, context)
+
+    const getTabBarContent = () => {
+        if (tabIndex === '0') {
+            return <><RangePicker
+                size='small'
+                format='DD-MM-YYYY'
+                disabledDate={(current) => disabledDate(current)}
+                onCalendarChange={(value) => {
+                    setDates(value)
+                }}
+            />
+                <Button size='small'>
+                    <DownloadOutlined onClick={() => onConfirm()}/>
+                </Button></>
+        } else if (tabIndex === '1') {
+            return <Space>
+                {
+                    access && <>  <DatePicker
+                        disabledDate={(date) => handleCashBackDate(date)}
+                        onChange={handleMonthChange} picker="month"/>
+                        <CashBackButton month={month}
+                                        year={year}/></>
+                }
+            </Space>
+        } else if (tabIndex === '2') {
+            return <DatePicker onChange={handleMonthChange} picker="month"/>
+        }
+    }
+
+
+    return (
+        <Card size='small' className='card-body-0 border-top-blue'>
+            <Tabs
+                tabBarExtraContent={getTabBarContent()}
+                defaultActiveKey={tabIndex}
+                onChange={(e) => setTabIndex(e)}
+            >
+                <TabPane tab='ICICI Bank Outgoing' key={'0'}>
+                    <ICICIBankOutgoing/>
+                </TabPane>
+                {access && <TabPane tab='Transaction Fee' key={'1'}>
+                    <CashBack
+                        month={month}
+                        year={year}
+                    />
+                </TabPane>}
+                {fuelCashback_access &&
+                <TabPane tab='Reliance' key={'2'}>
+                    {isNil(month) || isNil(year) ?
+                        <> <h4 align='center'>Reliance Fuel CashBack</h4></> :
+                        <RelianceCashBack month={month} year={year} setMonth={setMonth} setYear={setYear}/>
+                    }
+                </TabPane>
+                }
+
+            </Tabs>
+        </Card>
+    )
 }
 
 export default PayablesContainer
