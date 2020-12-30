730c418645be22f820e8cfcf3d3f28445ca33bcd
Karthikeyan <>
2020-12-30 11:36:56 +0530

    reliance code review

--- src/components/partners/payables/reliancecashback/RelianceCashBack.tsx
+++ src/components/partners/payables/reliancecashback/RelianceCashBack.tsx
@@ -1,16 +1,13 @@
 
-import { Table, message, Row, Col, Space, Button } from 'antd';
-import LabelWithData from '../../../common/labelWithData';
-
-import React, { useState } from 'react';
-import isNil from 'lodash/isNil'
+import { Table, message, Row, Col, Button } from 'antd'
+import React, { useState, useEffect } from 'react'
 import get from 'lodash/get'
 import Link from 'next/link'
-import { gql, useMutation, useQuery } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import Process from './process'
-import useShowHideWithRecord from '../../../../hooks/useShowHideWithRecord';
+import useShowHideWithRecord from '../../../../hooks/useShowHideWithRecord'
 import now from 'lodash/now'
-
+import useShowHide from '../../../../hooks/useShowHide'
 
 const reliance_cashback = gql`
 query reliance_cashback${now()}($year: Int!, $month: Int!) {
@@ -29,106 +26,101 @@ query reliance_cashback${now()}($year: Int!, $month: Int!) {
   `
 
 const RelianceCashBack = (props) => {
-    let { month, year} = props
-    const [relianceCashbackDetails, setRelianceCashbackDetails] = useState([])
+  const { month, year } = props
+  console.log('month, year', month, year)
+  const [relianceCashbackDetails, setRelianceCashbackDetails] = useState([])
+  const initial = { processVisible: false }
 
-    const { loading, error, data } = useQuery(
-        reliance_cashback, {
-        variables: { year, month },
-        fetchPolicy: "network-only",
-        notifyOnNetworkStatusChange: true,
-        onCompleted(data) {
-            setRelianceCashbackDetails(get(data, 'reliance_cashback', []))
-          },
-        onError(error) {
-            setRelianceCashbackDetails([])
-            message.error(error.message.toString())
-        },
+  const { visible, onShow, onHide } = useShowHide(initial)
+
+  const { loading, error, data } = useQuery(
+    reliance_cashback, {
+      variables: { year, month },
+      fetchPolicy: 'network-only',
+      notifyOnNetworkStatusChange: true,
+      skip: !month
     }
-    )
+  )
 
-    const initial = { visible: false }
+  let _data = {}
+  if (!loading) {
+    _data = data
+  }
+  if (error) {
+    message.error(error.message.toString())
+  }
 
-    const { object, handleHide, handleShow } = useShowHideWithRecord(initial)
+  useEffect(() => {
+    const reliance_cashback = get(_data, 'reliance_cashback', [])
+    setRelianceCashbackDetails(reliance_cashback)
+  }, [loading])
 
+  const columns = [
+    {
+      title: 'Partner Code',
+      dataIndex: 'cardcode',
+      render: (text, record) => {
+        return (
+          <Link href='/partners/[id]' as={`/partners/${text}`}>
+            {text}
+          </Link>
+        )
+      }
+    },
+    {
+      title: 'Consumption',
+      dataIndex: 'consumption'
+    },
+    {
+      title: 'CashBack',
+      dataIndex: 'amount'
+    },
+    {
+      title: 'Percentage',
+      dataIndex: 'percentage'
+    },
+    {
+      title: 'Balance',
+      dataIndex: 'balance',
+      render: (text, record) => text.toFixed(2)
+    }
 
-    
-    const columns = [
-        {
-            title: 'Partner Code',
-            dataIndex: 'cardcode',
-            render: (text, record) => {
-                return <Link href='/partners/[id]' as={`/partners/${text}`}>
-                    {text}
-                </Link>
+  ]
+  return (
+    <div>
+      <Table
+        rowClassName={(record, index) => record.balance > 0 ? 'cashbackRow' : 'cashbackRowNegativeWallet'}
+        columns={columns}
+        dataSource={relianceCashbackDetails}
+        size='small'
+        scroll={{ x: 1156 }}
+        pagination={false}
+        rowKey={(record) => record.cardcode}
+        loading={loading}
+      />
+      {relianceCashbackDetails.length > 0 &&
+        <Row>
+          <Col xs={24} className='text-right p10'>
+            <Button
+              type='primary'
+              onClick={() => onShow('processVisible')}
+            >Next
+            </Button>
+          </Col>
+        </Row>}
 
-            }
-        },
-        {
-            title: 'Consumption',
-            dataIndex: 'consumption'
-        },
-        {
-            title: 'CashBack',
-            dataIndex: 'amount'
-        },
-        {
-            title: 'Percentage',
-            dataIndex: 'percentage'
-        },
-        {
-            title: 'Balance',
-            dataIndex: 'balance',
-            render: (text, record) => text.toFixed(2)
-        }
-
-    ];
-    return (
-        <div>
-            {true && <div> <Table
-                rowClassName={(record, index) => record.balance > 0 ? 'cashbackRow' : 'cashbackRowNegativeWallet'}
-                columns={columns}
-                dataSource={relianceCashbackDetails}
-                size='small'
-                scroll={{ x: 1156 }}
-                pagination={false}
-                rowKey={(record) => record.cardcode}
-            />
-                <br />
-                <Row gutter={10} className='item'>
-                    <Col flex='1800px' className='text-right'>
-                        <Space>
-                            <LabelWithData
-                                label=''
-                                margin_bottom
-                                data={
-                                    relianceCashbackDetails.length > 0 && <Button type="primary"
-                                        onClick={() => handleShow('visible', '', '', {})} >Next</Button>
-                                }
-                                labelSpan={1}
-                            />
-                        </Space>
-                    </Col>
-                </Row>
-            </div>
-            }
-
-            {object.visible && (
-                <Process
-                    visible={object.visible}
-                    onHide={handleHide}
-                    title={'Reliance CashBack'}
-                    month={month}
-                    year={year}
-                    setRelianceCashbackDetails={setRelianceCashbackDetails}
-                />
-            )}
-        </div>
-
-
-    )
+      {visible.processVisible && (
+        <Process
+          visible={visible.processVisible}
+          onHide={onHide}
+          title='Reliance CashBack'
+          month={month}
+          year={year}
+          setRelianceCashbackDetails={setRelianceCashbackDetails}
+        />
+      )}
+    </div>
+  )
 }
-
-
 
 export default RelianceCashBack
--- src/components/partners/payables/reliancecashback/process.tsx
+++ src/components/partners/payables/reliancecashback/process.tsx
@@ -1,15 +1,15 @@
-import { Modal, Form, Input, message, Button, DatePicker } from 'antd';
+import { Modal, message, DatePicker, Space } from 'antd'
 import { useState, useContext } from 'react'
 import { gql, useMutation, useQuery } from '@apollo/client'
 import get from 'lodash/get'
 import userContext from '../../../../lib/userContaxt'
-import moment from 'moment';
+import moment from 'moment'
 import Loading from '../../../common/loading'
 import now from 'lodash/now'
 
 const process = 'RELIANCE_FUEL_CASHBACK'
 const currentDate = moment().format('YYYYMMDD')
-const period_status = "Locked"
+const period_status = 'Locked'
 
 const GET_TOKEN_AND_CLOSING_PERIOD = gql`
 query get_token${now()}($ref_id: Int!, $process: String!, $period_status: String!) {
@@ -31,63 +31,68 @@ mutation process_reliance_cashback($year:Int! ,$month:Int!,$created_by:String!,$
 }
 `
 
-
 const Process = (props) => {
-
-  const { visible, title, onHide, month, year,setRelianceCashbackDetails } = props
+  const { visible, title, onHide, month, year, setRelianceCashbackDetails } = props
   const [posting_date, setPosting_date] = useState(currentDate)
 
   const { loading, data, error } = useQuery(GET_TOKEN_AND_CLOSING_PERIOD, { variables: { ref_id: parseInt(currentDate), process, period_status }, fetchPolicy: 'network-only' })
+  if (error) {
+    message.error(error.message.toString())
+  }
 
   const context = useContext(userContext)
 
   const [processCashback, { loading: mutationLoading }] = useMutation(
     process_cashback, {
-    onError(error) {
-      message.error(`Error Occured!!`)
-      onHide()
-    },
-    onCompleted(data) {
-      const status = get(data, 'process_reliance_cashback.status', null)
-      if (status === 'OK') {
-        message.success(get(data, 'process_reliance_cashback.description', 'Transaction Processed'))
+      onError (error) {
+        message.error(error.toString() || 'Error Occured!!')
         onHide()
-        setRelianceCashbackDetails([])
-      } else {
-        message.error(`Error Occured!!`)
-        onHide()
+      },
+      onCompleted (data) {
+        const status = get(data, 'process_reliance_cashback.status', null)
+        if (status === 'OK') {
+          message.success(get(data, 'process_reliance_cashback.description', 'Transaction Processed'))
+          onHide()
+          setRelianceCashbackDetails([])
+        } else {
+          message.error('Error Occured!!')
+          onHide()
+        }
       }
-    }
-  })
+    })
   const disabledDate = (current) => {
-    let closing_period = get(data, 'closing_period_status.posting_from_date', moment())
-    const start = moment(closing_period, 'YYYY-MM-DD');
-    return current < start || current > moment();
+    const closing_period = get(data, 'closing_period_status.posting_from_date', moment())
+    const start = moment(closing_period, 'YYYY-MM-DD')
+    return current < start || current > moment()
   }
 
   const onSubmit = () => {
     processCashback({ variables: { month, year, posting_date, created_by: context.email, token: data.token, process } })
   }
   const onChange = (date, dateString) => {
-    console.log('dateString', date)
     setPosting_date(date.format('YYYYMMDD'))
   }
 
-  return <Modal
-    title={title}
-    visible={visible}
-    onCancel={onHide}
-    confirmLoading={loading}
-    okText={'Process'}
-    onOk={() => onSubmit()}
-  >
-    PostingDate : <DatePicker defaultValue={moment()} onChange={onChange}
-      disabledDate={(current) => disabledDate(current)}
-
-    />
-    {(loading || mutationLoading) &&
-      <Loading fixed />}
-  </Modal>
+  return (
+    <Modal
+      title={title}
+      visible={visible}
+      onCancel={onHide}
+      confirmLoading={loading}
+      okText='Process'
+      onOk={() => onSubmit()}
+    >
+      <Space>
+        <label>PostingDate : </label>
+        <DatePicker
+          defaultValue={moment()} onChange={onChange}
+          disabledDate={(current) => disabledDate(current)}
+        />
+      </Space>
+      {(loading || mutationLoading) &&
+        <Loading fixed />}
+    </Modal>
+  )
 }
 
 export default Process
