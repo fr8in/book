e865cefc01d528430e4cc145dcc09cd1ad6d03a5
Shilpaveronica <shilpa.v@fr8.in>
2021-01-09 14:19:44 +0530

    session time out

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,7 +3,19 @@ import { WebSocketLink } from '@apollo/client/link/ws'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { setContext } from '@apollo/client/link/context'
 import { refreshToken } from './auth'
+import { onError } from '@apollo/client/link/error'
+import u from '../lib/util'
 // import WebSocket from 'ws';
+
+const { application_error } = u
+
+const changeSubscriptionToken = token => {
+  if (wsLink.subscriptionClient.connectionParams.authToken === token) {
+  wsLink.subscriptionClient.connectionParams.authToken = token
+  wsLink.subscriptionClient.close()
+  wsLink.subscriptionClient.connect()
+}
+}
 
 const isBrowser = typeof window !== 'undefined'
 const wsLink = isBrowser ? new WebSocketLink({
@@ -12,8 +24,9 @@ const wsLink = isBrowser ? new WebSocketLink({
     lazy: true,
     reconnect: true,
     connectionParams: async () => {
-      refreshToken()
+      await refreshToken()
       const token = await localStorage.getItem('token')
+      console.log('web',token)
       if (token) {
         return {
           headers: {
@@ -29,9 +42,9 @@ const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_HTTP_CORE,
   credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
 })
-const authLink = setContext((_, { headers }) => {
+const authLink = setContext( async(_, { headers }) => {
   // get the authentication token from local storage if it exists
-  refreshToken()
+  await refreshToken()
   const token = localStorage.getItem('token')
   // return the headers to the context so httpLink can read them
   if(token){
@@ -43,6 +56,8 @@ const authLink = setContext((_, { headers }) => {
   }}
 })
 
+const concatedHttpLink = authLink.concat(httpLink)
+
 const link = isBrowser ? split(
   ({ query }) => {
     const definition = getMainDefinition(query)
@@ -52,15 +67,30 @@ const link = isBrowser ? split(
     )
   },
   wsLink,
-  httpLink
-) : httpLink
+  concatedHttpLink
+) : concatedHttpLink
 
-export default function createApolloClient (initialState, ctx) {
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  console.log('graphQLErrors', graphQLErrors)
+    console.log('networkError', networkError)
+    const networkErrorMessage = networkError && networkError.message ? networkError.message : null
+    const graphqlErrorMessage = graphQLErrors && graphQLErrors.length > 0 && graphQLErrors[0].message ? graphQLErrors[0].message : null
+    console.log(networkErrorMessage, graphqlErrorMessage)
+      if ((networkError && networkErrorMessage.includes(application_error.JWT_TOKEN_EXPIRE_ERROR)) || 
+      (graphQLErrors && graphqlErrorMessage.includes(application_error.JWT_TOKEN_EXPIRE_ERROR))) {
+        console.log('jwterror in')
+        refreshToken(true)
+      }
+})
+
+const createApolloClient = (initialState, ctx) => {
   // The `ctx` (NextPageContext) will only be present on the server.
   // use it to extract auth headers (ctx.req) or similar.
   return new ApolloClient({
     ssrMode: Boolean(ctx),
-    link: authLink.concat(link),
+    link: errorLink.concat(link),
     cache: new InMemoryCache().restore(initialState)
   })
 }
+
+export default { changeSubscriptionToken, createApolloClient}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import Router from 'next/router'
 import { message } from 'antd'
 import jwt from 'jsonwebtoken'
 import moment from 'moment'
-
+import  changeSubscriptionToken  from './apolloClient'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
@@ -83,7 +83,7 @@ const signOut = async () => {
   }
 }
 
-const refreshToken = () => {
+const refreshToken = (reload) => {
   const token = localStorage.getItem('token')
   if(token){
   const decodedToken = jwt.decode(token)
@@ -96,11 +96,15 @@ const refreshToken = () => {
     firebase.auth().onAuthStateChanged(async user => {
       if (user) {
         const token = await user.getIdToken(true)
+        changeSubscriptionToken(token)
         localStorage.setItem('token', token)
       }
     })
+    if(reload){
+      window.location.reload()
+    }
   }
-}
+ }
 }
 
 export { auth, signInWithGoogle, signOut, refreshToken }
