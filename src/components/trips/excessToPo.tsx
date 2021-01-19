ee4b03f5bb9d96070faab28ec97ec9885e0e7806
Shilpaveronica <Shilpaveronica@bitbucket.org>
2021-01-19 14:06:33 +0530

    inprogress

--- src/components/trips/excessToPo.tsx
+++ src/components/trips/excessToPo.tsx
@@ -5,42 +5,30 @@ import { gql, useQuery } from '@apollo/client'
 import ConfirmPo from './confirmPo'
 import get from 'lodash/get'
 
-const PARTNER_SEARCH_QUERY = gql`query partner_search($search: String){
-search_partner(args:{search:$search, status_ids: "{4}"}){
+const PARTNER_SEARCH_QUERY = gql`
+query partner_search($search: String){
+ search_partner(args:{search:$search, status_ids: "{4}"}){
     id
     link
     description
   }
 }`
 
-const PARTNER_FOR_PO_QUERY = gql`query truck_po_creation($id: Int!) {
-  truck(where: {id: {_eq: $id}}) {
-    id
-    truck_no
-    truck_type {
-      id
-      name
-    }
-    partner {
-      id
-      cardcode
-      name
-    }
-  }
-}`
-
 const ExcessToPo = (props) => {
   const { visible, onHide, record } = props
 
-  const initial = { search: null, partner_id: null, po_visible: false, truck_id: null }
-  const [obj, setObj] = useState(initial)
-
+  // const initial = { search: null, partner_id: null, po_visible: false, truck_id: null }
+  // const [obj, setObj] = useState(initial)
+  const [search,setSearch] = useState(null)
+  const [partnerId,setPartnerId] = useState(null)
+  const [poVisible,setPoVisible] = useState(false)
+  const [truckId,setTruckId] = useState(null)
 
   const { loading, error, data } = useQuery(
     PARTNER_SEARCH_QUERY,
     {
-      variables: { search: obj.search || '' },
-      skip: !obj.search,
+      variables: { search: search || '' },
+      skip: !search,
       fetchPolicy: 'cache-and-network',
       notifyOnNetworkStatusChange: true
     }
@@ -53,43 +41,28 @@ const ExcessToPo = (props) => {
   }
   const partnerSearch = get(_data, 'search_partner', [])
 
-  const { loading:partnerPoLoading, error:partnerPoError, data:partnerPoData } = useQuery(
-    PARTNER_FOR_PO_QUERY,
-    {
-      variables: { id:obj.truck_id},
-      skip: !obj.truck_id,
-      fetchPolicy: 'cache-and-network',
-      notifyOnNetworkStatusChange: true
-    }
-  )
-
-  console.log('TrucksForPO Error', error)
-  let _partnerPoData = {}
-  if (!partnerPoLoading) {
-    _partnerPoData = partnerPoData
-  }
   const [form] = Form.useForm()
 
-  const truck = get(_partnerPoData, 'truck[0]', [])
-  const partner_list = get(truck, 'partner', null)
-
-
   const onPartnerSearch = (value) => {
-    setObj({ ...obj, search: value })
+    setSearch(value)
   }
 
   const onPartnerSelect = (value, partner) => {
-    setObj({ ...obj, partner_id: partner.key, po_visible: (obj.truck_id && partner.key) })
+    setPartnerId(partner.key)
+    setPoVisible(truckId && partner.key)
   }
 
   const onTruckSelect = (truck, partner) => {
-    setObj({ ...obj, truck_id: truck.id, po_visible: (truck.id ||  obj.partner_id) })
+    setTruckId(truck.id)
+    setPoVisible(truck.id || partnerId)
   }
   
 
   const onClosePoModal = () => {
-    setObj(initial)
+    setPoVisible(false)
   }
+
+  console.log('search',search,'partnerId',partnerId,'poVisible',poVisible,'truckId',truckId)
   return (
     <>
       <Modal
@@ -107,25 +80,25 @@ const ExcessToPo = (props) => {
               onSearch={onPartnerSearch}
               onChange={onPartnerSelect}
             >
-               {partner_list && 
+               {/* {partner_list && 
           <Select.Option key={partner_list.id} value={partner_list.name}>{partner_list.name}</Select.Option>
-        }
+        } */}
               {partnerSearch && partnerSearch.map(_part => (
                 <Select.Option key={_part.id} value={_part.description}>{_part.description}</Select.Option>
               ))}
             </Select>
           </Form.Item>
-          <TrucksForPO onChange={onTruckSelect} partner_id={obj.partner_id} />
+          <TrucksForPO onChange={onTruckSelect} partner_id={partnerId} />
         </Form>
       </Modal>
-      {obj.po_visible &&
+      {/* {obj.po_visible &&
         <ConfirmPo
           visible={obj.po_visible}
           truck_id={obj.truck_id}
           record={record}
           onHide={onClosePoModal}
           hideExess={onHide}
-        />}
+        />} */}
     </>
   )
 }
--- src/components/trips/trucksForPO.tsx
+++ src/components/trips/trucksForPO.tsx
@@ -5,86 +5,68 @@ import { useState } from 'react'
 
 
 const TRUCKS_FOR_PO_QUERY = gql`
-query partner_po_creation($id:Int!){
-  partner(where:{id:{_eq:$id}}){
+query trucks_for_po($partner_id: Int, $truck_search: String) {
+  truck(where: {partner_id: {_eq: $partner_id}, truck_status: {name: {_eq: "Waiting for Load"}}, truck_no: {_ilike: $truck_search}}) {
     id
-    cardcode
-    name
-    trucks(where:{truck_status:{name:{_eq:"Waiting for Load"}}}){
+    truck_no
+    truck_type {
       id
-      truck_no
-      truck_type{
-        id
-        name
-      }
+      name
+    }
+    partner {
+      name
+      id
     }
   }
-}`
-
-const TRUCKS_SEARCH_QUERY = gql `query truck_search($search: String){
-  search_truck(args:{search:$search, status_ids: "{5}"}){
-      id
-      link
-      description
-    }
-  }`
+}
+`
 
 const TrucksForPO = (props) => {
   const { partner_id, onChange } = props
-  const initial = { search: null }
-  const [obj, setObj] = useState(initial)
 
-
-  const { loading:truck_loading, error:truck_error, data:truck_data } = useQuery(
-    TRUCKS_SEARCH_QUERY,
-    {
-      variables: { search: obj.search || '' },
-      skip: !obj.search,
-      fetchPolicy: 'cache-and-network',
-      notifyOnNetworkStatusChange: true
-    }
-  )
-
-  let _truck_data = {}
-  if (!truck_loading) {
-    _truck_data = truck_data
-  }
-  const truckSearch = get(_truck_data, 'search_truck', [])
+  const [truckSearch, setTruckSearch] = useState(null)
+  console.log('partner_id', partner_id,'!partner_id', !partner_id,'truckSearch', truckSearch,'!truckSearch',!truckSearch)
 
   const { loading, error, data } = useQuery(
     TRUCKS_FOR_PO_QUERY,
     {
-      variables: { id: partner_id },
-      skip: !partner_id,
+      variables: {
+        partner_id: partner_id || null,
+        truck_search: truckSearch 
+      },
+      skip: !partner_id ,
       fetchPolicy: 'cache-and-network',
       notifyOnNetworkStatusChange: true
     }
   )
-
   console.log('TrucksForPO Error', error)
+  console.log('TrucksForPO data', data)
   let _data = {}
   if (!loading) {
     _data = data
   }
   const [form] = Form.useForm()
 
-  const partner = get(_data, 'partner[0]', null)
-  const truck_list = get(partner, 'trucks', [])
-
+  
+  const truck_list = get(_data, 'truck', [])
+console.log('truck_list',truck_list)
   const onTruckSelect = (value, Item) => {
-    let selectd_truck;
+    let selected_truck;
     let truck;
-  if(truck_list.length > 0){
-    selectd_truck = truck_list && truck_list.filter(t => t.id === parseInt(Item.key, 10))
-    truck = selectd_truck && selectd_truck.length > 0 && selectd_truck[0] 
-  } else if(truckSearch.length > 0) {
-    selectd_truck = truckSearch && truckSearch.filter(t => t.id === parseInt(Item.key, 10))
-    truck = selectd_truck && selectd_truck.length > 0 && selectd_truck[0] 
-  }
-    onChange(truck, partner)
+    if (truck_list.length > 0) {
+      selected_truck = truck_list && truck_list.filter(truck => truck.id === parseInt(Item.key, 10))
+      truck = selected_truck && selected_truck.length > 0 && selected_truck[0]
+    }
+     else if (truckSearch.length > 0) {
+      selected_truck = truckSearch && truckSearch.filter(truck => truck.id === parseInt(Item.key, 10))
+      truck = selected_truck && selected_truck.length > 0 && selected_truck[0]
+    }
+    console.log('truck',truck)
+    onChange(truck)
   }
   const onTruckSearch = (value) => {
-    setObj({ ...obj, search: value })
+    console.log('value',value)
+    setTruckSearch(value)
   }
   return (
     <Form.Item name='truck'>
@@ -98,9 +80,6 @@ const TrucksForPO = (props) => {
         {truck_list && truck_list.map(_truck => (
           <Select.Option key={_truck.id} value={_truck.truck_no}>{_truck.truck_no}</Select.Option>
         ))}
-        {truckSearch && truckSearch.map(_part => (
-                <Select.Option key={_part.id} value={_part.description}>{_part.description}</Select.Option>
-              ))}
       </Select>
     </Form.Item>
   )
