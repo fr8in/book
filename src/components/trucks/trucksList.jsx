b73bd9995603abc82e3d450a0cf1c630810d2f93
Magaarajah <magaarajah.g@fr8.in>
2021-01-29 14:27:55 +0530

    fix

--- src/components/trucks/trucksList.jsx
+++ src/components/trucks/trucksList.jsx
@@ -12,9 +12,10 @@ const TrucksList = (props) => {
     const [copy, setCopy] = useState(false)
   
     const groupedData = _.groupBy(record, function(item) { return item.truck_type.code})
-   
+    const branch_name = !isEmpty(data) ? data[0].name : null 
+
     const getMessage = (groupedData) => {
-        let   message = 'FR8 Trucks available at  \n \n';
+        let   message = `FR8 Trucks available at ${branch_name} \n \n`;
         let key = Object.keys(groupedData)
         key.forEach((type)=> {
           message = message + `${type} \n \n`
