65e2be7627aeb42a42da8fd498ec32bd7967c4b4
saarukaa <saarukaa.devi@fr8.in>
2021-01-30 09:55:00 +0530

    fix

--- src/process.js
+++ src/process.js
@@ -2,7 +2,6 @@ const input_data = require('./input.jsx')
 const _ = require('lodash')
 const {get} = require('lodash')
 
-const { database } = require('firebase')
 //1-- loop through branch map
 //2-- get all trucks under one branch using flatmap
 //3-- group by truck type 
@@ -22,12 +21,8 @@ return trucksGroupedByTruckType
 
 const formatMessage=(truck,i) =>
 {
-    const _formattedMessage =  `${i++}) Partner: ${get(truck, 'partner.name')}
-    Truck No: ${truck.truck_no} - ${get(truck, 'tat')} hrs
-    O: ${get(truck, 'partner.partner_users[0].mobile','-')} / D: ${get(truck, 'trips[0].driver.mobile','-')}
-    Comment: ${get(truck, 'last_comment.description','-')}
-    
-    `
+    const _formattedMessage= `${i++}) Partner: ${get(truck, 'partner.name')}
+    Truck No: ${truck.truck_no} - ${get(truck, 'tat')} hrs\nO: ${get(truck, 'partner.partner_users[0].mobile','-')} / D: ${get(truck, 'trips[0].driver.mobile','-')}\nComment: ${get(truck, 'last_comment.description','-')}\n\n`
     return _formattedMessage
   }
 
@@ -35,21 +30,24 @@ const getMessage = (groupedTrucks) => {
     // let   message = `FR8 Trucks available at ${branch_name} \n \n`;
    
     const truck_types = Object.keys(groupedTrucks)
-    
-    let message = truck_types.map(truck_type=> {
-    `${message} 
+    let message =''
+     message = truck_types.map(truck_type=>
+      { 
+        return(
+    `${message}
 
     ${truck_type}:
     ${groupedTrucks[truck_type].map(formatMessage)}
-    `}) 
+    `)}) 
     return message
       
 }
 
 const trucks= flatMapTruck(input_data[0].region[0].branches)
 const groupedTrucks = groupByTruckType(trucks)
-const message = getMessage(groupedTrucks)
+export const message = getMessage(groupedTrucks)
 console.log('prop', message)
 
 
    
+
