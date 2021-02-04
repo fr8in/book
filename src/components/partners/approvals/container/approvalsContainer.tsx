1e17690de7787edde16e218e13a44c2197082077
Shilpaveronica <Shilpaveronica@bitbucket.org>
2021-02-04 18:36:55 +0530

    credit history fix

--- src/components/partners/approvals/container/approvalsContainer.tsx
+++ src/components/partners/approvals/container/approvalsContainer.tsx
@@ -1,9 +1,8 @@
-import Pending from '../pending'
-import Approved from '../approvedAndRejected'
 import TransferToBankHistory from '../../../customers/transferToBankHistory'
 import { Tabs, Card } from 'antd'
 import Incentive from '../incentive'
 import AdditionalAdvanceBankApproval from '../additionalAdvanceBank';
+import CreditDebit from '../creditDebit'
 
 const TabPane = Tabs.TabPane
 
@@ -11,19 +10,16 @@ const Approvals = () => {
   return (
     <Card size='small' className='card-body-0 border-top-blue'>
       <Tabs>
-        <TabPane tab='Credit' key='1'>
-          <Pending />
+      <TabPane tab='Credit' key='1'>
+          <CreditDebit />
         </TabPane>
-        <TabPane tab='History' key='2'>
-          <Approved />
-        </TabPane>
-        <TabPane tab='Transfer to Bank' key='3'>
+        <TabPane tab='Transfer to Bank' key='2'>
          <TransferToBankHistory />
         </TabPane>
-        <TabPane tab='Incentive' key='4'>
+        <TabPane tab='Incentive' key='3'>
           <Incentive />
         </TabPane> 
-        <TabPane tab='Additional Advance Bank' key='5'>
+        <TabPane tab='Additional Advance Bank' key='4'>
           <AdditionalAdvanceBankApproval />
         </TabPane>
       </Tabs>
