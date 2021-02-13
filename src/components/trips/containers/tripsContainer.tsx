6261bf308e87e3cc7477e0a75273cd78ff4d9672
Devavarshni M B <devavarshni.mb@fr8.in>
2021-02-13 14:03:44 +0530

    fix

--- src/components/trips/containers/tripsContainer.tsx
+++ src/components/trips/containers/tripsContainer.tsx
@@ -72,15 +72,27 @@ const TripsContainer = () => {
 
   const aggrigation = {
     all_trip: { _and: [{ trip_status: { name: { _in: ['Delivered', 'Invoiced', 'Paid', 'Recieved', 'Closed'] } } }] },
+
     delivered_trip: { _and: [{ trip_status: { name: { _in: ['Delivered'] } }, pod_verified_at: { _is_null: true } }], customer: { name: { _ilike: countFilter.delivered_customername ? `%${countFilter.delivered_customername}%` : null } }, partner: { name: { _ilike: countFilter.delivered_partnername ? `%${countFilter.delivered_partnername}%` : null } },branch: {region_id: {_in: (state.regions && state.regions.length > 0) ? state.regions : null}},
     branch_id: {_in: (state.branches && state.branches.length > 0) ? state.branches : null},
     source_connected_city_id: {_in:(state.cities && state.cities.length > 0) ? state.cities : null },
     truck_type_id: {_in: (state.types && state.types.length > 0) ? state.types : null},
     branch_employee_id: {_in:(state.managers && state.managers.length > 0) ? state.managers : null },
     truck: { truck_no: { _ilike: countFilter.delivered_truckno ? `%${countFilter.delivered_truckno}%` : null } } },
-    pod_verified_trip: { _and: [{ trip_status: { name: { _in: ['Delivered'] } }, pod_verified_at: { _is_null: false } }], customer: { name: { _ilike: countFilter.pod_verified_customername ? `%${countFilter.pod_verified_customername}%` : null } }, partner: { name: { _ilike: countFilter.pod_verified_partnername ? `%${countFilter.pod_verified_partnername}%` : null } }, truck: { truck_no: { _ilike: countFilter.pod_verified_truckno ? `%${countFilter.pod_verified_truckno}%` : null } } },
+
+    pod_verified_trip: { _and: [{ trip_status: { name: { _in: ['Delivered'] } }, pod_verified_at: { _is_null: false } }], customer: { name: { _ilike: countFilter.pod_verified_customername ? `%${countFilter.pod_verified_customername}%` : null } }, partner: { name: { _ilike: countFilter.pod_verified_partnername ? `%${countFilter.pod_verified_partnername}%` : null } },
+    branch_id: {_in: (state.branches && state.branches.length > 0) ? state.branches : null},
+    source_connected_city_id: {_in:(state.cities && state.cities.length > 0) ? state.cities : null },
+    truck_type_id: {_in: (state.types && state.types.length > 0) ? state.types : null},
+    branch_employee_id: {_in:(state.managers && state.managers.length > 0) ? state.managers : null },
+    truck: { truck_no: { _ilike: countFilter.pod_verified_truckno ? `%${countFilter.pod_verified_truckno}%` : null } } },
+
     invoiced_trip: { _and: [{ trip_status: { name: { _in: ['Invoiced', 'Paid', 'Recieved', 'Closed'] } }, pod_dispatched_at: { _is_null: true } }], customer: { name: { _ilike: countFilter.invoiced_customername ? `%${countFilter.invoiced_customername}%` : null }, payment_manager: { name: { _in: !isEmpty(filter) ? filter : null } } } },
     partner: { name: { _ilike: countFilter.invoiced_partnername ? `%${countFilter.invoiced_partnername}%` : null } },
+    branch_id: {_in: (state.branches && state.branches.length > 0) ? state.branches : null},
+    source_connected_city_id: {_in:(state.cities && state.cities.length > 0) ? state.cities : null },
+    truck_type_id: {_in: (state.types && state.types.length > 0) ? state.types : null},
+    branch_employee_id: {_in:(state.managers && state.managers.length > 0) ? state.managers : null },
     truck: { truck_no: { _ilike: countFilter.invoiced_truckno ? `%${countFilter.invoiced_truckno}%` : null } }
   }
 
